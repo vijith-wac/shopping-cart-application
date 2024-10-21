9d1638429374dc089d058a02b6551e087f385148
vijithhg <vijithhgedu@gmail.com>
2024-10-21 23:19:49 +0530

    first commit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const upload = require('../middleware/multer')
+const { addProduct, getAllProduct, getProductById, updateProductById, deleteProductById } = require('../controllers/productController')
+
+router.post('/',upload.single('image'),addProduct)
+router.get('/', getAllProduct)
+router.get('/:id', getProductById)
+router.put('/:id', updateProductById)
+router.delete('/:id', deleteProductById)
+
+
+
+module.exports = router
